2a6c13f1ff1d1480e0ca480a62651e822da58383
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-04-27 09:41:31 +0430

    Add redux basic structure for rockets section with store file

---
+++ src/components/redux/rockets/rocketsSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+
+};
+
+export const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducers: {
+  },
+});
+
+export default rocketsSlice.reducer;
